f846c254b2cfaf1259b49e7aac1e1dab5dfcedbd
Masoud Moawiye <mnmoawiye@my.waketech.edu>
2025-01-14 13:21:31 -0500

    Commited

---
+++ index.js
@@ -0,0 +1,30 @@
+// JavaScript code 
+console.log("Internal JS consol message.")
+console.log(document)
+
+
+window.onload = function(){
+   alert("Hello World")
+}
+
+document.getElementById("myH1").innerHTML = "I love JavaScript";
+document.getElementById('myParagragh').textContent = 'Hello, this is a paragragh'
+
+
+
+
+
+//Styles
+document.body.style.color ='green';
+document.h1.style.backGroundColor = 'black';
+
+
+console.log("test")
+1st pic = document.getElementById("mmm")
+pic.height = "500"
+
+// EVENT LISTENING //
+document.getElementById("MyHeader").addEventListener('click', function{});
+    // function goes here
+    document .getElementById
+    
